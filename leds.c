582e160e087dcc2ceab2d0bdc8b2e5aa76a0c3e0
Hrushikesh Patwardhan <hrushikeshp13@gmail.com>
2026-05-19 11:00:37 +0530

    fix led library

--- leds.c
+++ leds.c
@@ -36,7 +36,7 @@ void configure_led(void)
     led_strip_config_t strip_config = {
         .strip_gpio_num = LED_STRIP_BLINK_GPIO,   // The GPIO that connected to the LED strip's data line
         .max_leds = LED_STRIP_LED_NUMBERS,        // The number of LEDs in the strip,
-        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
+        //.led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
         .led_model = LED_MODEL_WS2812,            // LED strip model
         .flags.invert_out = true,                // whether to invert the output signal
     };
